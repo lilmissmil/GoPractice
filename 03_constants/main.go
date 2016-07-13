3181ed07917a2a4fe69c5593f77f9afe3c99435e
Milie Fang <milie@outlook.com>
2016-07-13 11:11:51 -0500

    added constants git status

---
+++ 03_constants/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//const p string = "death & taxes"
+//const q = 42
+//They can be in package scope or block scope depending on usage
+//A constant is a simple unchanging value
+//You can name more than one constant as shown below with Pi and Language
+
+const (
+	Pi       = 3.14
+	Language = "Go"
+)
+
+//The above is also known as multiple initialization
+
+func main() {
+	const p string = "death & taxes"
+	const q = 42
+	fmt.Println("p -", p)
+	fmt.Println("q -", q)
+	fmt.Println("Pi -", Pi)
+	fmt.Println("Language -", Language)
+
+}
