911a4630892778810a31c5e95d662ed82acc310f
Milie Fang <milie@outlook.com>
2016-07-13 11:14:09 -0500

    added constants folder

--- 03_constants/main.go
+++ 03_constants/main.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-//const p string = "death & taxes"
-//const q = 42
-//They can be in package scope or block scope depending on usage
-//A constant is a simple unchanging value
-//You can name more than one constant as shown below with Pi and Language
+// Constants can be in package scope or block scope depending on usage
+// A constant is a simple unchanging value
+// You can name more than one constant as shown below with Pi and Language
 
 const (
 	Pi       = 3.14
 	Language = "Go"
 )
 
-//The above is also known as multiple initialization
+// The above is also known as multiple initialization
 
 func main() {
 	const p string = "death & taxes"
